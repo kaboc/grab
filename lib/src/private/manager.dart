a78e74eb22363787f2c1da3b6c71ae0eeeb699e4
Kabo <20254485+kaboc@users.noreply.github.com>
2024-01-25 21:32:41 +0900

    Update analysis_options.yaml

--- lib/src/private/manager.dart
+++ lib/src/private/manager.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: public_member_api_docs
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
--- test/common/widgets.dart
+++ test/common/widgets.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_key_in_widget_constructors
-
 import 'package:flutter/material.dart';
 
 class TestStatelessWidget extends StatelessWidget {
